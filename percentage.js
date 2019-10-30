4bb7a91a2f9e1a851433e6f21c2baaff79c8f6fd
Ata Gülalan <xava@hotmail.com.tr>
2019-10-30 21:29:45 +0300

    Update percentage.js

--- percentage.js
+++ percentage.js
@@ -41,8 +41,6 @@ function getPercentage(callback) {
 getPercentage((device, percentage) => {
 	percentage = percentage > 100 ? 100 : percentage < 0 ? 0 : percentage
 
-	console.log('Initializing...')
-
 	console.log(percentage === 0
 		? `Please connect your SteelSeries Wireless Device first.`
 		: `${percentage}% battery left on your ${device.product}`)
